7e09452a5215995903e2396a9fd46df0fbf6985a
rapPayne <bob@creator.net>
2024-10-16 08:06:30 -0500

    Added lab 4 - useContext

---
+++ solutions/04_useContext/src/components/Login.tsx
@@ -0,0 +1,39 @@
+import { ReactElement, useId, useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { toast } from 'react-hot-toast';
+import { login as loginToServer } from '../data/authentication.ts';
+import { User } from '../types/User.ts';
+
+interface Props {
+  setUser: (user: User) => void
+}
+export const Login = ({ setUser }: Props): ReactElement => {
+  const id = useId();
+  const navigate = useNavigate();
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  return (
+    <>
+      <h1>Login</h1>
+      <Link to="/register">Register</Link>
+      <div>
+        <label htmlFor={`username${id}`}>Username</label>
+        <input id={`username${id}`} onChange={e => setUsername(e.target.value)} value={username} />
+      </div>
+      <div>
+        <label htmlFor={`password${id}`}>Password</label>
+        <input id={`password${id}`} onChange={e => setPassword(e.target.value)} value={password} />
+      </div>
+      <button onClick={login}>Log in</button>
+    </>
+  )
+
+  function login(): void {
+    loginToServer(username, password)
+      .then(r => (console.log(r), r))
+      .then(user => setUser(user))
+      .then(() => navigate('/cart'))
+      .catch(err => toast.error(`Can't log in. ${err.message}`))
+  }
+}
---
+++ solutions/04_useContext/src/components/Order.tsx
@@ -0,0 +1,52 @@
+import { ReactElement, useContext, useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { getMenuItems, getOrder } from "../data/repository";
+import { getNumberOfDiners, getOrderTotal, toCurrency } from "../data/utilities";
+import './Order.css';
+import { Order as OrderType } from "../types/Order";
+import { MenuItem } from "../types/MenuItem";
+import { UserContext } from "../App";
+
+
+export const Order = (): ReactElement => {
+  const orderId: number = +(useParams().orderId || 0);
+  const [order, setOrder] = useState<OrderType>();
+  const [menuItems, setMenuItems] = useState<Array<MenuItem>>();
+  const user = useContext(UserContext);
+  useEffect(() => {
+    getOrder(orderId)
+      .then(o => setOrder(o))
+      .catch(err => console.log(err));
+    getMenuItems()
+      .then(mi => setMenuItems(mi));
+  }, [orderId]);
+  user;  // Just there to suppress linter warnings.
+  return (
+    <section className="Order">
+      <h2>Order {orderId}</h2>
+      <p><span className="label">Customer:</span> {order?.userId}</p>
+      <p><span className="label">Number of guests:</span> {order && getNumberOfDiners(order)}</p>
+      <p><span className="label">Credit card:</span> {order?.creditCard?.pan}, <span className="label">expiry:</span> {order?.creditCard?.expiryMonth}/{order?.creditCard?.expiryYear}</p>
+      <p><span className="label">Area:</span> {order?.area}</p>
+      <p><span className="label">Location:</span> {order?.location}</p>
+      <p><span className="label">Order time:</span> {order?.orderTime?.toLocaleString()}</p>
+      <table>
+        <tbody>
+          {order?.items?.map(item => (
+            <tr key={item.id} className="orderLine">
+              <td>{item.itemID && getMenuItem(item.itemID)?.name}</td>
+              <td>{toCurrency(item.price ?? 0)}</td>
+              <td>(for {item.firstName})</td>
+            </tr>
+          ))}
+          <tr className="taxLine"><td>Tax</td><td>{toCurrency(order?.tax ?? 0)}</td></tr>
+          <tr className="tipLine"><td>Tip</td><td>{toCurrency(order?.tip ?? 0)}</td></tr>
+          <tr className="totalLine"><td>Total</td><td>{order && getOrderTotal(order)}</td></tr>
+        </tbody>
+      </table>
+    </section>
+  )
+  function getMenuItem(id: number) {
+    return menuItems?.find(mi => mi.id === id)
+  }
+}
