b7edbe985b0f4a8fc6aa770660cc9eef1b4b5208
rapPayne <bob@creator.net>
2024-10-20 11:05:01 -0500

    User passed to NavBar through context, not props

--- solutions/04_useContext/src/components/NavBar.tsx
+++ solutions/04_useContext/src/components/NavBar.tsx
@@ -1,11 +1,13 @@
+import { useContext } from 'react';
 import { Link } from 'react-router-dom';
 import { User } from '../types/User';
+import { UserContext } from '../App';
 
 interface Props {
-  user: User | undefined;
   setUser: (user: User | undefined) => void
 }
-export const NavBar = ({ user, setUser }: Props) => {
+export const NavBar = ({ setUser }: Props) => {
+  const user = useContext(UserContext);
   return (
     <nav>
       <Link to="/">Dinner and a movie</Link>
